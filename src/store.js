aa19f33a1f51dc6fd4728f9bc34fd8b61f0f8e2c
Denis Olekh <denisolekh@gmail.com>
2020-05-31 02:14:35 +0300

    hw

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import boardReducer from "./board/board.reducer";
-import { getItem, setItem } from "./board/board.storage";
 
 const reducer = combineReducers({
   board: boardReducer,
